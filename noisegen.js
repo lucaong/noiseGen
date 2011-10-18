720c5169cb35c621ecfce0bdb18e1eb1082c818d
Luca Ongaro <lukeongaro@gmail.com>
2011-10-18 12:07:00 +0300

    Better feature detection, add comments and depth option

--- noisegen.js
+++ noisegen.js
@@ -4,26 +4,31 @@
       width: 32,
       height: 32,
       opacity: 0.2,
-      fallbackImage: false
+      fallbackImage: false,
+      depth: 60
     },
     canvas = document.createElement("canvas"),
-    ctx = canvas.getContext("2d"),
-    x = 0,
-    y = 0,
     options = $.extend(defaultOptions, options);
-    if (!ctx) {
+    if (!canvas.getContext || !canvas.getContext("2d")) {
+      // Canvas not supported :(
       if (!!options.fallbackImage) {
+        // Fallback image provided, set it as background
         return this.css("background-image", "url(" + options.fallbackImage + ")");
       } else {
+        // Fallback background image not provided, just return maintaining chainability
         return this;
       }
     } else {
+      // Canvas supported :)
+      var ctx = canvas.getContext("2d"),
+      x = 0,
+      y = 0;
       canvas.width = options.width;
       canvas.height = options.height;
       while (x < canvas.width) {
         y = 0;
         while (y < canvas.height) {
-          number = Math.floor(Math.random() * 60);
+          number = Math.floor(Math.random() * options.depth);
           ctx.fillStyle = "rgba(" + number + "," + number + "," + number + "," + options.opacity + ")";
           ctx.fillRect(x, y, 1, 1);
           y++;
